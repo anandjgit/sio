a943da7b6916730fa11095691edddf40c7c41b58
Anand <57168260+anandjgit@users.noreply.github.com>
2020-12-23 19:50:26 +0530

    build

---
+++ precache-manifest.ca09fef979de15c11953b8449c88277c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be63725df542c6328fdb8e5efd056daf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77b1caa0a88c8fc5cee",
+    "url": "/static/css/2.305790f0.chunk.css"
+  },
+  {
+    "revision": "246080ba957c58d85c49",
+    "url": "/static/css/main.503d8c9e.chunk.css"
+  },
+  {
+    "revision": "b77b1caa0a88c8fc5cee",
+    "url": "/static/js/2.8ae8e750.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8ae8e750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246080ba957c58d85c49",
+    "url": "/static/js/main.f919caed.chunk.js"
+  },
+  {
+    "revision": "914e25f363399f3225f0",
+    "url": "/static/js/runtime-main.a919462b.js"
+  },
+  {
+    "revision": "7799dece2c79854f63f09e7dfa528b88",
+    "url": "/static/media/boxed-bg.7799dece.jpg"
+  },
+  {
+    "revision": "b9de27c7821f6ee5c6f91a244cc572e4",
+    "url": "/static/media/color-wheel.b9de27c7.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
